89c558a28006341ee4cb643e38f99c08c57eb119
Robert Konecny <rokorobot@users.noreply.github.com>
2026-01-16 22:37:05 +0100

    Configure for Netlify Static Export

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   images: {
-    /*
-    {
-      protocol: 'https',
-      hostname: 'cdn.sanity.io',
-    },
-    */
+    unoptimized: true,
   },
 };
 
